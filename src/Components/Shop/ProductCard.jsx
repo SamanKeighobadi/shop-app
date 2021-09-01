9412fed87dbbf17a209c69b4bc30517edb096fca
saman keighobadi <saman.keighobadi@gmail.com>
2021-09-01 18:49:21 +0430

    create button animation component

--- src/Components/Shop/ProductCard.jsx
+++ src/Components/Shop/ProductCard.jsx
@@ -1,10 +1,11 @@
 import React from "react";
-import { Card, Image, Icon, Button } from "semantic-ui-react";
+import { Card, Image, Icon, Button, Label } from "semantic-ui-react";
+import ProductCardButton from "./ProductCardButton";
 
 const ProductCard = ({ title, image, price, category }) => {
   return (
     <div>
-      <Card color="violet"  >
+      <Card color="violet">
         <Image size="small" centered src={image} wrapped ui={true} />
         <Card.Content>
           <Card.Header> {title} </Card.Header>
@@ -16,11 +17,8 @@ const ProductCard = ({ title, image, price, category }) => {
           </Card.Description>
         </Card.Content>
         <Card.Content extra>
-          <a>
-            <Icon name="dollar" />
-            {price}
-          </a>
-          <Button size='mini' color="violet">Shop</Button>
+          <Label floating color='red'>{price}</Label>
+          <ProductCardButton />
         </Card.Content>
       </Card>
     </div>
---
+++ src/Components/Shop/ProductCardButton.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { Button, Icon } from "semantic-ui-react";
+
+const ProductCardButton = () => {
+  return (
+    <div>
+      <Button animated color="violet" floated="right" size="mini">
+        <Button.Content visible>Add To Cart</Button.Content>
+        <Button.Content hidden>
+          <Icon name="shop" />
+        </Button.Content>
+      </Button>
+    </div>
+  );
+};
+
+export default ProductCardButton;
