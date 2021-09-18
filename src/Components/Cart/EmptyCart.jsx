05f79b31226453b8309285208d5d488e6f991d2e
saman keighobadi <saman.keighobadi@gmail.com>
2021-09-18 17:40:34 +0430

    change empty cart style

--- src/Components/Cart/EmptyCart.jsx
+++ src/Components/Cart/EmptyCart.jsx
@@ -1,18 +1,25 @@
 import React from "react";
 //? React semantic UI Component
-import { Header, Grid } from "semantic-ui-react";
+import { Header, Grid,Image,Container,Button } from "semantic-ui-react";
+import {Link} from 'react-router-dom'
 
 const EmptyCart = () => {
   return (
     <div>
       <Grid textAlign="center" style={{ marginTop: "4rem" }}>
         <Grid.Column verticalAlign="middle">
-          <Header
-            color="red"
-            content=" Your Cart is Empty"
-            size="huge"
-            style={{ fontSize: "4rem" }}
-          />
+        <Container >
+        <Image src='https://static.thenounproject.com/png/538404-200.png'   alt='empty  cart'  centered 
+        size='small' />
+        <Header content='Your cart is empty' />
+        <Header content='You can see more products by click on link blow' size='tiny' />
+        <Link to='/shop'>
+        <Button size='tiny'  color='violet' >Products</Button>
+        </Link>
+        <Button basic color='violet' size='tiny'>
+          Home
+        </Button>
+        </Container>
         </Grid.Column>
       </Grid>
     </div>
