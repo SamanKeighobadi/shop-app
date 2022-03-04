cc1ec971b85db332a0ea0dc0dbec85a06a096777
SamanKeighobadi <saman.keighobadi@gmail.com>
2022-03-04 21:17:11 +0330

    feat: create slider
    init swiper slider for home page

--- src/Components/Home/AppHome.jsx
+++ src/Components/Home/AppHome.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 //? React Semantic UI Components
 import { Header, Container, Segment } from "semantic-ui-react";
+import AppSlider from "../utils/AppSlider";
 
 const AppHome = () => {
   return (
     <Container fluid textAlign="center">
-      <Segment placeholder>
+      {/* <Segment placeholder>
         <Header content="Home Page" size="huge" />
         <Container textAlign="justified" style={{ width: "30vw" }}>
           <p>
@@ -15,7 +16,8 @@ const AppHome = () => {
             eiusmod laborum mollit velit velit
           </p>
         </Container>
-      </Segment>
+      </Segment> */}
+      <AppSlider />
     </Container>
   );
 };
