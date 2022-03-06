0c0dc3422af1d52872dad7e38632573a78cff339
SamanKeighobadi <saman.keighobadi@gmail.com>
2022-03-06 17:22:13 +0330

    feat: validation filed login form

--- src/Components/Authentication/Login.jsx
+++ src/Components/Authentication/Login.jsx
@@ -10,6 +10,9 @@ import {
 } from "semantic-ui-react";
 //? Import React Helmet
 import { Helmet } from "react-helmet";
+//? Formik
+import { Formik } from "formik";
+import { logniShcema } from "./Validation/LoginValidationSchema";
 
 const Login = () => {
   return (
@@ -19,23 +22,59 @@ const Login = () => {
       </Helmet>
       <Container style={{ marginTop: "4rem" }}>
         <Segment color="violet">
-          <Form>
-            <Header content="Login" size={"huge"} />
-            <Form.Field>
-              <label>Email:</label>
-              <input type="email" placeholder="example@gmail.com" />
-            </Form.Field>
-            <Form.Field>
-              <label>Password:</label>
-              <input type="password" placeholder="Password" />
-            </Form.Field>
-            <Form.Field>
-              <Checkbox label="I agree to the Terms and Conditions" />
-            </Form.Field>
-            <Button type="submit" color="vk" size="small">
-              Submit
-            </Button>
-          </Form>
+          <Formik
+            initialValues={{ email: "", password: "" }}
+            validationSchema={logniShcema}
+            onSubmit={(values) => {
+              alert(JSON.stringify(values))
+            }}
+          >
+            {({
+              values,
+              touched,
+              errors,
+              handleChange,
+              handleBlur,
+              handleSubmit,
+            }) => (
+              <Form onSubmit={handleSubmit}>
+                <Header content="Login" size={"huge"} />
+                <Form.Field error={touched.email && errors.email ? true:false}>
+                  <label>Email:</label>
+                  <input
+                    type="email"
+                    name="email"
+                    onChange={handleChange}
+                    onBlur={handleBlur}
+                    value={values.email}
+                    placeholder="example@gmail.com"
+                    
+                  />
+                  {touched.email && errors.email ? (
+                    <div style={{color:'#9f3a38',padding:'4px 2px'}}>{errors.email}</div>
+                  ) : null}
+                </Form.Field>
+                <Form.Field error={touched.password && errors.password ? true:false}>
+                  <label>Password:</label>
+                  <input
+                    type="password"
+                    name="password"
+                    onChange={handleChange}
+                    onBlur={handleBlur}
+                    value={values.password}
+                    placeholder="Password"
+                  />
+                  {touched.password && errors.password ? (
+                    <div style={{color:'#9f3a38',padding:'4px 2px'}}>{errors.password}</div>
+                  ) : null}
+                </Form.Field>
+              
+                <Button type="submit" color="vk" size="small">
+                  Submit
+                </Button>
+              </Form>
+            )}
+          </Formik>
         </Segment>
       </Container>
     </Fragment>
